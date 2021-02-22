6a11803961424aaee9ee186f0f6136d82bc3e965
Samuel Ramos <samuelsantos7@yahoo.com.br>
2021-02-22 17:23:51 -0300

    initial app

---
+++ src/app.js
@@ -0,0 +1,22 @@
+const app = require('express')()
+const http = require('http').Server(app)
+
+const io = require('socket.io')(http)
+
+app.get('/', (request, response) => {
+  response.sendFile(__dirname + '/index.html')
+})
+
+io.on('connection', (socket) => {
+  console.log('Usuário conectado!')
+
+  socket.on('chat message', (message) => {
+    console.log('Mensagem: ' + message)
+  })
+
+  socket.on('disconnect', () => {
+    console.log('Usuário desconectado!')
+  })
+})
+
+http.listen(3333, () => console.log('Listening on port 3333'))
